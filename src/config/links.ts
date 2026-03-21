d77860802e656dd53bd6cf96b56f160e5985ea01
MH-Al0 <136981957+MH-Al0@users.noreply.github.com>
2026-03-21 01:12:47 +0100

    Update Discord link in Links configuration

--- src/config/links.ts
+++ src/config/links.ts
@@ -2,5 +2,5 @@
 export const Links = {
   Modrinth: 'https://modrinth.com/plugin/graceac',
   Github:   'https://github.com/MH-Al0/GraceAC-Public',
-  Discord:  'https://discord.gg/8tkCamWPTdD',
+  Discord:  'https://discord.gg/GSj4pxCZyx',
 } as const
